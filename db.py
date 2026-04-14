021e7fb782887bf00c60213f7a4007bba1ed3944
Biswajit Panda <biswajitpanda104@gmail.com>
2026-04-15 00:10:17 +0530

    Debug DB error

--- db.py
+++ db.py
@@ -4,18 +4,18 @@ import os
 def get_connection():
     try:
         conn = pymysql.connect(
-            host=os.getenv("gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"),
-            user=os.getenv("hA15cqijeRCKvtm.root"),
-            password=os.getenv("LOH1mQykOMKvAyEt"),
-            database=os.getenv("test"),
-            port=int(os.getenv("4000")),
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT")),
             ssl={"ssl": {}}
         )
         return conn
 
     except Exception as e:
-        print("DB Error ❌:", e)
-        return None
+        print("DB Error ❌:", e)   # ✅ print error in logs
+        return None               # ✅ return None ONLY
 
 
 # Test connection
