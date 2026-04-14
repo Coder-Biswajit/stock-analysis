f38a18529f8347f9abc0e0b37e585b8cd7652386
Biswajit Panda <biswajitpanda104@gmail.com>
2026-04-14 23:55:19 +0530

    Connected TiDB database

--- db.py
+++ db.py
@@ -1,19 +1,24 @@
 import pymysql
+import os
 
 def get_connection():
     try:
         conn = pymysql.connect(
-            host="localhost",
-            user="root",
-            password="root",
-            database="stock_app"
+            host=os.getenv("gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"),
+            user=os.getenv("hA15cqijeRCKvtm.root"),
+            password=os.getenv("LOH1mQykOMKvAyEt"),
+            database=os.getenv("test"),
+            port=int(os.getenv("4000")),
+            ssl={"ssl": {}}
         )
         return conn
+
     except Exception as e:
-        print("Error:", e)
+        print("DB Error ❌:", e)
         return None
 
 
+# Test connection
 if __name__ == "__main__":
     conn = get_connection()
     if conn:
